51bc3812d6ae81dfc2b611f50bfcfca398de2ee7
frogmonkee <118191517+frogmonkee-uniswap@users.noreply.github.com>
2023-07-31 21:25:05 -0400

    pricing function

--- src/Exchange.sol
+++ src/Exchange.sol
@@ -16,6 +16,27 @@ contract Exchange {
         token.transferFrom(msg.sender, address(this), _tokenAmount);
     }
 
+    function getTokenAmount(uint256 _etherSold) public view returns(uint256) {
+        require(_etherSold > 0, "ethSold is too small");
+        uint256 tokenReserve = getReserve();
+        return getAmount(_etherSold, address(this).balance, tokenReserve);
+    }
+
+    function getEtherAmount(uint256 _tokenSold) public view returns(uint256) {
+        require(_tokenSold > 0, "tokenSold is too small");
+        uint256 tokenReserve = getReserve();
+        return getAmount(_tokenSold, tokenReserve, address(this).balance);
+    }
+
+    function getAmount(
+    uint256 inputAmount,
+    uint256 inputReserve,
+    uint256 outputReserve
+    ) private pure returns (uint256) {
+    require(inputReserve > 0 && outputReserve > 0, "invalid reserves");
+    return (inputAmount * outputReserve) / (inputReserve + inputAmount);
+    }
+
     function getReserve() public view returns(uint256) {
         return IERC20(tokenAddress).balanceOf(address(this));
     }
