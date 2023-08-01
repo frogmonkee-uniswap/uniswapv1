81758f0d5f7a25d39bd7cbfcc44de70479ca2c22
frogmonkee <118191517+frogmonkee-uniswap@users.noreply.github.com>
2023-07-31 21:37:13 -0400

    pricing function test

--- test/AddLiquidity.sol
+++ test/AddLiquidity.sol
@@ -24,8 +24,18 @@ contract AddLiquidty is Test {
         vm.prank(userA);
         token.approve(address(exchange), 1000);
         vm.prank(userA);
+        // xy=k
+        // 2*500=1000
         exchange.addLiquidity{ value: 2 ether }(500);
         assertEq(address(exchange).balance, 2e18);
         assertEq(exchange.getReserve(), 500);
+
+        // Expect 166 as output
+        // (2 + 1) * (500 - 166) = 1000
+        console.log(exchange.getTokenAmount(1e18));
+
+        // Expect 0.666 as output
+        // (2 - 0.666) * (500 + 250) = 1000
+        console.log(exchange.getEtherAmount(250));
     }
 }
