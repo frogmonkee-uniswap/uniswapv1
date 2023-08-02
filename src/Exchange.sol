6c451172cb510bbb4486affe759f216f7a77e545
frogmonkee <118191517+frogmonkee-uniswap@users.noreply.github.com>
2023-08-02 19:27:37 -0400

    LP tokens and fees. No tests

--- src/Exchange.sol
+++ src/Exchange.sol
@@ -2,21 +2,68 @@
 pragma solidity ^0.8.13;
 
 import "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
-contract Exchange {
+contract Exchange is ERC20 {
     address public tokenAddress;
+    IERC20 public token;
 
-    constructor(address _token){
+    constructor(address _token) ERC20("uniswapv1", "UNI") {
         require(_token != address(0), "Invalid token address");
         tokenAddress = _token;
+        token = IERC20(tokenAddress);
     }
 
-    function addLiquidity(uint256 _tokenAmount) public payable {
-        IERC20 token = IERC20(tokenAddress);
-        token.transferFrom(msg.sender, address(this), _tokenAmount);
+    function addLiquidity(uint256 _tokenAmount) public payable returns(uint256) {
+        // If pool has not been initialized, then accept tokens + ETH
+        if(getReserve() == 0) {
+            // Transfer tokens from user's wallet to Exchange contract
+            token.transferFrom(msg.sender, address(this), _tokenAmount);
+            // mint LP tokens proportioal to ETH deposited
+            uint256 liquidity = address(this).balance;
+            _mint(msg.sender, liquidity);
+            return liquidity;
+        } else {
+            // If pool exist, it must follow price curve. Enforce price of token based on msg.value
+            uint256 ethReserve = address(this).balance - msg.value;
+            uint256 tokenReserve = getReserve();
+            uint256 tokenAmount = (msg.value * tokenReserve) / address(this).balance;
+            require(_tokenAmount >= tokenAmount, "Deposit more tokens");
+            // Transfers exact tokenAmount and leaves _tokenAmount - tokenAmount in the user's wallet
+            token.transferFrom(msg.sender, address(this), tokenAmount);
+            // amountMinted = totalAmount * (ethReserve / ethDeposited​)
+            uint256 liquidity = totalSupply() * (msg.value / ethReserve);
+            _mint(msg.sender, liquidity);
+            return liquidity;
+        }
     }
 
-    function getTokenAmount(uint256 _etherSold) public view returns(uint256) {
+    function removeLiquidity(uint256 _amount) public returns(uint256, uint256) {
+        require(_amount > 0, "invalid amount");
+
+        // EthAmount = (ethReserve * amountLP) / totalAmountLP​
+        uint256 ethAmount = (address(this).balance * _amount) / totalSupply();
+        // EthAmount = (tokenReserve * amountLP) / totalAmountLP​
+        uint256 tokenAmount = (getReserve() * _amount) / totalSupply();
+
+        _burn(msg.sender, _amount);
+        payable(msg.sender).transfer(ethAmount);
+        token.transfer(msg.sender, tokenAmount);
+
+        return (ethAmount, tokenAmount);
+    }
+
+    function getAmount(
+        uint256 inputAmount,
+        uint256 inputReserve,
+        uint256 outputReserve
+        ) private pure returns (uint256) {
+        require(inputReserve > 0 && outputReserve > 0, "invalid reserves");
+        uint256 inputAmountWithFee = inputAmount * 99;
+        return (inputAmountWithFee * outputReserve) / ((inputReserve * 100) + inputAmount);
+        }
+
+        function getTokenAmount(uint256 _etherSold) public view returns(uint256) {
         require(_etherSold > 0, "ethSold is too small");
         uint256 tokenReserve = getReserve();
         return getAmount(_etherSold, address(this).balance, tokenReserve);
@@ -26,15 +73,6 @@ contract Exchange {
         require(_tokenSold > 0, "tokenSold is too small");
         uint256 tokenReserve = getReserve();
         return getAmount(_tokenSold, tokenReserve, address(this).balance);
-    }
-
-    function getAmount(
-    uint256 inputAmount,
-    uint256 inputReserve,
-    uint256 outputReserve
-    ) private pure returns (uint256) {
-    require(inputReserve > 0 && outputReserve > 0, "invalid reserves");
-    return (inputAmount * outputReserve) / (inputReserve + inputAmount);
     }
 
     function ethToTokenSwap(uint _minAmount) public payable {
--- test/AddLiquidity.t.sol
+++ test/AddLiquidity.t.sol
@@ -11,32 +11,45 @@ contract AddLiquidty is Test {
     IERC20 public token;
 
     address userA;
+    address userB;
 
     function setUp() public {
         token = new ERC20("frogmonkee", "FROG");
+        exchange = new Exchange(address(token));
+
         userA = makeAddr("User A");
         vm.deal(userA, 2 ether);
         deal(address(token), userA, 10000);
 
-        exchange = new Exchange(address(token));
+        userB = makeAddr("User B");
+        vm.deal(userB, 2 ether);
+        deal(address(token), userB, 10000);
     }
     
     function testAddLiquidity() public {
         vm.prank(userA);
-        token.approve(address(exchange), 1000);
+        token.approve(address(exchange), 10000);
         vm.prank(userA);
         // xy=k
         // 2*500=1000
         exchange.addLiquidity{ value: 2 ether }(500);
+        // Assert balance is 2 ETH
         assertEq(address(exchange).balance, 2e18);
+        // Assert 500 tokens are in the exchange contract
         assertEq(exchange.getReserve(), 500);
+        // Assert received 2 LP tokens in return
+        assertEq(exchange.balanceOf(userA), 2e18);
 
-        // Expect 166 as output
-        // (2 + 1) * (500 - 166) = 1000
-        console.log(exchange.getTokenAmount(1e18));
-
-        // Expect 0.666 as output
-        // (2 - 0.666) * (500 + 250) = 1000
-        console.log(exchange.getEtherAmount(250));
+        vm.prank(userB);
+        token.approve(address(exchange), 10000);
+        vm.prank(userB);
+        exchange.addLiquidity{ value: 2 ether }(300);
+        // Assert 250 tokens are added to the exchange, even though 300 were sent
+        assertEq(exchange.getReserve(), 750);
+        // Assert received 2 LP tokens in return 
+        // amountMinted = totalAmount * (ethReserve / ethDeposited​)
+        assertEq(exchange.balanceOf(userB), 2e18);
+        // Assert exchange balance is 4 ETH
+        assertEq(address(exchange).balance, 4e18);
     }
 }
